36028e53e9393f5d9d93944f2047ae543c4eff2a
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2024-09-30 20:40:16 -0700

    added routes and handlers for microservice operations

--- application/routes.go
+++ application/routes.go
@@ -4,6 +4,7 @@ import(
 	"net/http"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/MattM27538/Microservice1/handler"
 )
 
 func loadRoutes() *chi.Mux{
@@ -12,5 +13,17 @@ func loadRoutes() *chi.Mux{
 	router.Get("/", func(w http.ResponseWriter, r*http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
+
+	router.Route("/orders", loadOrderRoutes)
 	return router
+}
+
+func loadOrderRoutes(router chi.Router){
+	orderHandler:=&handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
